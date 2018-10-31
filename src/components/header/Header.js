db5f138a021cf6208db68d41d93e87a25999a8e1
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-10-31 04:04:08 -0500

    resolviendo merging del header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,15 +5,9 @@ class Header extends Component {
     return (
       <div className="header">
         <header>
-<<<<<<< HEAD
-          <nav class="navbar navbar-light bg-light nav-login">
-            <a class="navbar-brand" href="AmericaTV">
-              <img src="src/img/Frame.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
-=======
           <nav className="navbar navbar-light bg-light nav-login">
             <a className="navbar-brand " href="AmericaTV">
               <img src="frame.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
->>>>>>> d18377c1863bd7070b9f34cb1e8c04b1ae07604d
               AMÉRICA COMERCIAL
             </a>
           </nav>
