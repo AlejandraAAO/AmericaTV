f876c83670c238f9fd30aa326e3f145f7b4acae3
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-10-31 07:31:28 -0500

    arreglando estilos

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -41,7 +41,7 @@ class Calendar extends Component {
                    <th scope="row">{i < 10 ? ('0' + i) : i}:00 - {(i + 1) < 10 ? ('0' + (i + 1)) : (i + 1)}:00</th>
                    {
                      arrayDay.map((day, j) => {
-                       return (<td key={`day${j}`}><a href="#" id={i + "-" + day + "-" + data[day].name + "-" + data[day].price} onClick={this.handleClick.bind(this)}>{data[day].name}</a></td>)
+                       return (<td key={`day${j}`} className={`td text-secondary hour${i}`}><a href="#" id={i + "-" + day + "-" + data[day].name + "-" + data[day].price} onClick={this.handleClick.bind(this)}>{data[day].name}</a></td>)
                      })
                    }
                  </tr>
--- src/components/calendar/Form.js
+++ src/components/calendar/Form.js
@@ -1,111 +1,191 @@
-import React, { Component } from 'react';
-import Mini from './MiniCalendar'
+import React, { Component } from "react";
+import Mini from "./MiniCalendar";
 const database = window.firebase.database();
 class Form extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       products: [],
-      productName: '',
+      productName: "",
       productPrice: 0,
       surchargeAmount: 0,
       totalAmount: 0
-    }
+    };
     this.myRef = React.createRef();
   }
   componentDidMount() {
-    database.ref('/agency').once('value').then(snapshot => {
-      const arr = Object.values(snapshot.val().products);
-      this.setState({
-        products: arr
-      })
-    });
+    database
+      .ref("/agency")
+      .once("value")
+      .then(snapshot => {
+        const arr = Object.values(snapshot.val().products);
+        this.setState({
+          products: arr
+        });
+      });
     const hour = parseInt(this.props.hour);
     if (hour >= 12 && hour < 16) {
-      this.setState({ surchargeAmount: (this.props.price * 0.05) });
+      this.setState({ surchargeAmount: this.props.price * 0.05 });
     } else if (hour >= 16 && hour < 20) {
-      this.setState({ surchargeAmount: (this.props.price * 0.15) });
+      this.setState({ surchargeAmount: this.props.price * 0.15 });
     } else {
       this.setState({ surchargeAmount: 0 });
     }
   }
-  handleClick(schedule){
-    this.setState({ schedule: schedule});
+  handleClick(schedule) {
+    this.setState({ schedule: schedule });
     // console.log(this.state.schedule);
   }
   render() {
     return (
       <div>
         <form className="form-data p-5" onSubmit={this.handleSubmit.bind(this)}>
-          <Mini hour={this.props.hour}  handleClick={this.handleClick.bind(this)}/>
+          <Mini
+            hour={this.props.hour}
+            handleClick={this.handleClick.bind(this)}
+          />
           <div className="form-group">
-            <label htmlFor="exampleSelect1" className="bmd-label-floating">Producto</label>
-            <select className="form-control" id="exampleSelect1" onChange={this.handleChange.bind(this)}>
-              <option disabled selected>Selecciona una opción</option>
+            <label htmlFor="exampleSelect1" className="bmd-label-floating">
+              Producto
+            </label>
+            <select
+              className="form-control"
+              id="exampleSelect1"
+              onChange={this.handleChange.bind(this)}
+            >
+              <option disabled selected>
+                Selecciona una opción
+              </option>
               {this.state.products.map((element, i) => {
-                return (<option key={`products${i}`} value={element.price + "-" + element.name}>{element.name}</option>)
+                return (
+                  <option
+                    key={`products${i}`}
+                    value={element.price + "-" + element.name}
+                  >
+                    {element.name}
+                  </option>
+                );
               })}
             </select>
             <div className="invalid-feedback text-danger" id="if3">
               Debes ingresar un producto
-          </div>
+            </div>
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Programa</label>
-            <input type="text" className="form-control" value={this.props.name} disabled />
+            <input
+              type="text"
+              className="form-control"
+              value={this.props.name}
+              disabled
+            />
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Fecha</label>
-            <input type="text" className="form-control" value={this.props.day} disabled />
+            <input
+              type="text"
+              className="form-control"
+              value={this.props.day}
+              disabled
+            />
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Monto por producto</label>
-            <input type="text" className="form-control" value={this.state.productPrice} disabled />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.productPrice}
+              disabled
+            />
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Monto por programa</label>
-            <input type="text" className="form-control" value={this.props.price} disabled />
+            <input
+              type="text"
+              className="form-control"
+              value={this.props.price}
+              disabled
+            />
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Monto por recargo</label>
-            <input type="text" className="form-control" value={this.state.surchargeAmount} disabled />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.surchargeAmount}
+              disabled
+            />
           </div>
           <div className="form-group">
             <label className="bmd-label-floating">Monto total</label>
-            <input type="text" className="form-control" ref={this.myRef} value={(parseInt(this.state.productPrice) + parseInt(this.props.price) + parseInt(this.state.surchargeAmount))} disabled />
+            <input
+              type="text"
+              className="form-control"
+              ref={this.myRef}
+              value={
+                parseInt(this.state.productPrice) +
+                parseInt(this.props.price) +
+                parseInt(this.state.surchargeAmount)
+              }
+              disabled
+            />
           </div>
           <div className="form-check">
             <label>
-              <input type="checkbox" className="form-check-input" caria-label="Checkbox for following text input" required/> Acepto los términos y condiciones
+              <input
+                type="checkbox"
+                className="form-check-input"
+                caria-label="Checkbox for following text input"
+                required
+              />{" "}
+              Acepto los términos y condiciones
             </label>
           </div>
-          <button type="submit" className="btn btn-raised btn-warning btn-login" name="cancel">Cancelar</button>
-          <button type="submit" className="btn btn-raised btn-success btn-login" name="submit">Enviar</button>
+          <button
+            type="submit"
+            className="btn btn-raised btn-warning btn-login"
+            name="cancel"
+          >
+            Cancelar
+          </button>
+          <button
+            type="submit"
+            className="btn btn-raised btn-success btn-login"
+            name="submit"
+            id="submit"
+          >
+            Enviar
+          </button>
         </form>
+
       </div>
     );
   }
   handleChange(e) {
-    const if3= document.querySelector('#if3');
-    const newTarget = e.target.value.split('-');
-    this.setState({ productPrice: parseInt(newTarget[0]), productName: newTarget[1] });
-    if3.style.display="none";  
+    const if3 = document.querySelector("#if3");
+    const newTarget = e.target.value.split("-");
+    this.setState({
+      productPrice: parseInt(newTarget[0]),
+      productName: newTarget[1]
+    });
+    if3.style.display = "none";
   }
   handleSubmit(e) {
     e.preventDefault();
     if (this.state.productPrice !== 0) {
-      database.ref('booking/').push({
+      database.ref("booking/").push({
         name: this.props.name,
         productName: this.state.productName,
         productPrice: this.state.productPrice,
         programPrice: this.props.price,
         surchargePrice: this.state.surchargeAmount,
-        schedule:this.state.schedule,
+        schedule: this.state.schedule,
         totalPrice: this.myRef.current.value
       });
+     
     } else {
-      const if3= document.querySelector('#if3');
-      if3.style.display="block";
+      const if3 = document.querySelector("#if3");
+      if3.style.display = "block";
     }
   }
 }
